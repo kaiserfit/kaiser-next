c0a63309554e77890b02072289b91ba8f5238ee2
Kurk Villanueva <kurkboard@gmail.com>
2022-06-24 13:20:04 +0800

    fix animation
    from left to right

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,12 @@ module.exports = {
         },
         slideToLeft: {
           "0%": { position: "relative", right: 0 },
-          "100%": { right: "-100%" },
+          "100%": { position: "absolute", right: "300%" },
         },
       },
       animation: {
         teleportToRight: "teleportToRight 500ms ease-in-out 1",
-        slideToLeft: "slideToLeft 500ms ease-in-out 1",
+        slideToLeft: "slideToLeft 1200ms ease-in-out 1",
       },
     },
   },
